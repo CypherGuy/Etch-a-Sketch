8a155161de55356042c0fbb801a3fd5a72803fdd
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-20 13:47:50 +0000

    Seperate JS and HTML

---
+++ script.js
@@ -0,0 +1,64 @@
+const container = document.getElementById("grid-container");
+        let randomColor = false;
+
+        function randomColours() {
+            randomColor = !randomColor;
+        }
+
+        function requestSize() {
+            let size = parseInt(prompt("Enter size of grid from 1 to 100 here"));
+            console.log(size);
+            if (isNaN(size) || size >= 101) {
+                alert("Enter a number from 1 to 100");
+            } else {
+                clearGrid(size);
+            }
+        }
+
+        function clearGrid(size) {
+            container.innerHTML = ""; // Clear the grid container
+            makeGrid(size); // Recreate the grid with the specified size
+            updateResetButton(size);
+        }
+
+        function updateResetButton(size) {
+            const resetButton = document.getElementById("resetButton");
+            resetButton.setAttribute("onclick", `clearGrid(${size})`); // Set onclick attribute with the current size
+        }
+
+        function makeSquare(size) {
+            const squareDiv = document.createElement("div");
+            squareDiv.classList.add("square");
+            squareDiv.setAttribute("style", `--size: ${size};`);
+
+            squareDiv.addEventListener("mouseenter", function () {
+                if (randomColor) {
+                    squareDiv.classList.remove("permament-color");
+                    squareDiv.classList.add("random-color");
+                    squareDiv.style.backgroundColor = getRandomRGB();
+                } else {
+                    squareDiv.classList.remove("random-color");
+                    squareDiv.classList.add("permament-color");
+                    squareDiv.style.backgroundColor = "rgb(0, 255, 132)"; // This overrides the random colour on hover
+                }
+            });
+
+            return squareDiv;
+        }
+
+        function makeGrid(size) {
+            for (let i = 0; i < size ** 2; i++) {
+                container.appendChild(makeSquare(size));
+            }
+        }
+
+        function getRandomRGB() {
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+
+        let size = 16;
+        makeGrid(size);
+        updateResetButton(size);
